f518d879769aecebd0cda9c763412fc02975183c
sepmein <sepmein@gmail.com>
2024-08-28 11:03:38 +0800

    added data overview

---
+++ R/paths.R
@@ -0,0 +1,51 @@
+# ZAM ----
+## raw ----
+
+### DHIS2 ----
+PATH_ZAM_DH_HF_RAW_240723 <- "2_data/zam/1_raw/HMIS data Lukwipa and Kankumba.xls"
+PATH_ZAM_DH_VL_RAW_240723 <- "2_data/zam/1_raw/Weekly data.xlsx"
+
+### Survey ----
+PATH_ZAM_HH_HH_RAW_240723 <- "2_data/zam/1_raw/1,7_experiment_data_status.xlsx"
+PATH_ZAM_HH_CODEBOOK_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Survey- Codebook.xlsx"
+
+#### Baseline ----
+PATH_ZAM_HHB_HH_HOUSEINFO_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Baseline Survey Dataset/census_phone_combine.csv"
+PATH_ZAM_HHB_PR_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Baseline Survey Dataset/household_member_phone_combine.csv"
+PATH_ZAM_HHB_HH_DETAILED_HOUSEINFO_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Baseline Survey Dataset/household_phone_combine.csv"
+PATH_ZAM_HHB_HH_ITN_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Baseline Survey Dataset/net_phone_combine.csv"
+PATH_ZAM_HHB_HH_TEST_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Baseline Survey Dataset/Rufunsa 1,7mRCT BASE-LINE MICROSCOPY RESULTS final.xlsx"
+PATH_ZAM_HHB_HH_VISIT_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Baseline Survey Dataset/visit_phone_combine.csv"
+
+#### Endline ----
+PATH_ZAM_HHE_HH_HOUSEINFO_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Endline Survey Dataset/Endline-census.xlsx"
+PATH_ZAM_HHE_PR_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Endline Survey Dataset/Endline household_member_all.csv"
+PATH_ZAM_HHE_HH_DETAILED_HOUSEINFO_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Endline Survey Dataset/Endline-household.xlsx"
+PATH_ZAM_HHE_HH_ITN_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Endline Survey Dataset/Endline-net.xlsx"
+PATH_ZAM_HHE_HH_TEST_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Endline Survey Dataset/1,7 Parasite Microscope endline survey 2023 results_updated Rufunsa_version.xlsx"
+PATH_ZAM_HHE_HH_VISIT_RAW_240723 <- "2_data/zam/1_raw/Baseline and Endline Data/Endline Survey Dataset/Endline-visit.xlsx"
+
+## clean ----
+### DHIS2 ----
+PATH_ZAM_DH_HF <- "2_data/zam/2_clean/ZAM_DH_HF.qs"
+PATH_ZAM_DH_VL <- "2_data/zam/2_clean/ZAM_DH_VL.qs"
+
+### Survey ----
+PATH_ZAM_HH_HH <- "2_data/zam/2_clean/ZAM_HH_HH.qs"
+PATH_ZAM_HH_CODEBOOK <- "2_data/zam/2_clean/ZAM_HH_CODEBOOK.qs"
+
+#### Baseline ----
+PATH_ZAM_HHB_HH_HOUSEINFO <- "2_data/zam/2_clean/ZAM_HHB_HH_HOUSEINFO.qs"
+PATH_ZAM_HHB_PR <- "2_data/zam/2_clean/ZAM_HHB_PR.qs"
+PATH_ZAM_HHB_HH_DETAILED_HOUSEINFO <- "2_data/zam/2_clean/ZAM_HHB_HH_DETAILED_HOUSEINFO.qs"
+PATH_ZAM_HHB_HH_ITN <- "2_data/zam/2_clean/ZAM_HHB_HH_ITN.qs"
+PATH_ZAM_HHB_HH_TEST <- "2_data/zam/2_clean/ZAM_HHB_HH_TEST.qs"
+PATH_ZAM_HHB_HH_VISIT <- "2_data/zam/2_clean/ZAM_HHB_HH_VISIT.qs"
+
+#### Endline ----
+PATH_ZAM_HHE_HH_HOUSEINFO <- "2_data/zam/2_clean/ZAM_HHE_HH_HOUSEINFO.qs"
+PATH_ZAM_HHE_PR <- "2_data/zam/2_clean/ZAM_HHE_PR.qs"
+PATH_ZAM_HHE_HH_DETAILED_HOUSEINFO <- "2_data/zam/2_clean/ZAM_HHE_HH_DETAILED_HOUSEINFO.qs"
+PATH_ZAM_HHE_HH_ITN <- "2_data/zam/2_clean/ZAM_HHE_HH_ITN.qs"
+PATH_ZAM_HHE_HH_TEST <- "2_data/zam/2_clean/ZAM_HHE_HH_TEST.qs"
+PATH_ZAM_HHE_HH_VISIT <- "2_data/zam/2_clean/ZAM_HHE_HH_VISIT.qs"
